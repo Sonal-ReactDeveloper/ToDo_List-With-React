1e8cf60ee047cb992e1472b49d08981b5bdd177a
sonal <sonalsuresh2255@gmail.com>
2021-11-08 15:37:45 +0530

    App.js Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+  const [inputList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+
+    setItems((oldItems) => {
+        return [...oldItems, inputList];
+    });
+    
+  };
+
+
+  
+
+ return (
+   <>
+
+   <div className="main_div">
+     <div className="center_div">
+
+     </div>
+     <br/>
+     <h1>ToDo List</h1>
+     <br />
+
+     <input type="text" placeholder="Add a item " onChange={itemEvent} />
+
+     <button onClick={listOfItems}> + </button>
+
+     <ol>
+       
+
+       {Items.map((itemval) => {
+         return <li> {itemval} </li>
+       })}
+
+       
+     </ol>
+
+   </div>
+
+   </>
+
+ )
+
+};
 
 export default App;
